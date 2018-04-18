0c6bb92bf20fe85ce370191e1be3340e0923211d
Ziwei <zg48@cornell.edu>
2018-04-17 23:12:26 -0400

    upload new pythonapp

---
+++ pythonapp/application.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import networkx as nx
+import pandas as pd
+import scraper
+import similarity
+import randomwalk
+import show
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text1 = request.form['keyword']
+    text2 = request.form['depth']
+
+    root_page = text1
+    depth = int(text2)
+
+    if depth == 1: scraper.desc_1(root_page)
+    elif depth == 2: scraper.desc_2(root_page)
+    #plagiarismPercent = stringComparison.extremelySimplePlagiarismChecker(text1,text2)
+    docs = similarity.compute_similarity(text1 + '_1.json')
+
+    G = nx.Graph()
+    G.add_edges_from(docs)
+    G = G.to_directed()
+
+    path = randomwalk.DiGraphRandomWalk(G, 7, 10, .2, 'Linear algebra', True)
+    print('The paths are: ')
+    print(path)
+
+
+    show.graph(path)
+    return "<h1>Showing the result for  " + text1 + "</h1>"
+
+if __name__ == '__main__':
+    app.run()
