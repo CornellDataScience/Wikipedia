c02abe2d90bdd847dc4d5681e2b66c01f24c1d88
Ziwei Gu <zg48@cornell.edu>
2018-10-22 10:27:01 -0400

    Finished scrape_ontology script

--- web/scrape_ontology.py
+++ web/scrape_ontology.py
@@ -1,18 +1,19 @@
 import json
-from bs4 import BeautifulSoup as bs4
+from bs4 import BeautifulSoup
 import requests
 import csv
 import re
 import sys
 import io
 MAX_DEPTH = 3
+import scrape_json
 STEM = "https://en.wikipedia.org"
 
 def find(root_page):
     page = requests.get(root_page)
     parent_links = []
     try:
-        soup = bs4(page.text, "html5lib")
+        soup = BeautifulSoup(page.text, "html5lib")
         # text = soup.find_all('p')
         #extract text only before the "see also" section
         # see_also = soup.find('span', id='See_also')
@@ -30,9 +31,38 @@ def find(root_page):
     #     'text': full_text, 'desc_links': desc_links, 'desc_text':desc_text}
     return parent_links
 
+def visible(element):
+    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+        return False
+    elif re.match('<!--.*-->', str(element.encode('utf-8'))):
+        return False
+    return True
+
+def get_pages(category):
+    page = requests.get(category)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    mw_category = soup.select('#mw-pages .mw-category-group a')
+    acc = [i.get_text() for i in mw_category]
+    key = soup.select('#mw-pages a[href*=Category:]')
+    while True:
+        next_page = requests.get(STEM + key[-1]['href'])
+        next_soup = BeautifulSoup(next_page.content, 'html.parser')
+        next_mw_category = next_soup.select('#mw-pages .mw-category-group a')
+        acc += [i.get_text() for i in next_mw_category]
+        key = next_soup.select('#mw-pages a[href*=Category:]')
+        if key[-1].get_text() == 'previous page': # key is a list of length 2
+            break
+    return acc
+
 def scrape_articles(lst):
-    pass
+    pages_to_read = [get_pages(item) for item in lst] # 2D list
+
+    for i in range(len(pages_to_read)):
+        print('Reading page from Category ' + lst[i])
+        for word in pages_to_read[i]:
+            print(scrape_json.read_page('https://en.wikipedia.org/wiki/' + word))
 
 if __name__ == '__main__':
     root_page = str(sys.argv[1])
-    print(find(root_page))
+    # print(get_pages(root_page))
+    print(scrape_articles(find(root_page)))
