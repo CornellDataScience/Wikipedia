25d494d684e3cbac802fe5c3740268c0066186cd
Ziwei Gu <zg48@cornell.edu>
2018-12-04 13:57:35 -0500

    Update all.py

--- FA18/all.py
+++ FA18/all.py
@@ -1,14 +1,20 @@
-import similarity_new as sn
+import scraper_new as sc
+import similarity_new as sm
 import randomwalk as rw
 import makeGraph as mg
 import sys
 import json
 
 if __name__ == '__main__':
-    scraped_file = sys.argv[1]
-    titles, contents = sn.read_data('../data/' + scraped_file)
-    doc_term_matrix, vocabulary = sn.preprocess(contents)
-    sim_matrix = sn.create_similarity_matrix(doc_term_matrix, vocabulary)
+    root_page = sys.argv[1]
+    page_title = root_page.split("/")[-1]
+    links = sc.get_links(root_page)
+    titles_dict = sc.read_pages(links, page_title)
+    # Uncomment the following if sorting is needed:
+    # titles_dict = sorted(titles_dict.items(), key=lambda x: x[1], reverse=True)
+    titles, contents = sm.read_data('../data/' + page_title + '_raw_data.txt')
+    doc_term_matrix, vocabulary = sm.preprocess(contents)
+    sim_matrix = sm.create_similarity_matrix(doc_term_matrix, vocabulary)
     G = mg.make_prototype_graph(sim_matrix, titles)
     path, graph_path = rw.DiGraphRandomWalk(G, 20, 10, 0, {}, True)
     with open(scraped_file + '_path.json', 'w+') as outfile:
